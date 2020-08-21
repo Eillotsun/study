7b89c40574e2f80cc05a40448e6a0692ab052e8e
ssw <250853210@qq.com>
2020-08-21 16:14:44 +0800

    Initial Commit

---
+++ study/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  study
+//
+//  Created by 某某 s on 2020/8/21.
+//  Copyright © 2020 某某 s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
